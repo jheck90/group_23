b615b17e86a3c787c5e33e40b4492a4bc0c15179
John <casillaj@oregonstate.edu>
2018-07-21 13:33:22 -0600

    23 and 24

--- week4/sheet_23.c
+++ week4/sheet_23.c
@@ -1,3 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+//#include "dynamicArray.c"
+typedef DynArr dynArray;
 
 
 //https://oregonstate.instructure.com/courses/1714048/assignments/7297143?module_item_id=18107800
@@ -12,8 +17,9 @@ struct dynArrayIterator {
 void dynArrayIteratorInit (struct dynArray *da, struct dynArrayIterator *itr) {
    
 
-
-
+	assert(da);
+	itr->da = da;
+	itr->currentIndex = 0;
 
 
 }
@@ -21,13 +27,11 @@ void dynArrayIteratorInit (struct dynArray *da, struct dynArrayIterator *itr) {
 int dynArrayIteratorHasNext (struct dynArrayIterator *itr) {
 
 
+	assert(itr);									// Verify that iterator isn't null.
+	assert(itr->currentIndex >= 0);					// Verify that the current index isn't negative.
 
-
-
-
-
-
-
+	//return(itr->currentIndex < itr->da->size);	// Alternative return statement.
+	return(itr->currentIndex < sizeDynArr(itr->da));// Current value < size of the array.
 
 
 }
@@ -35,11 +39,10 @@ int dynArrayIteratorHasNext (struct dynArrayIterator *itr) {
 TYPE dynArrayIteratorNext (struct dynArrayIterator *itr) {
 
 
-
-
-
-
-
+	assert(itr);									// Verify that the iterator isn't null.
+	assert(itr->currentIndex >= 0);					// Verify that the current index isn't < 0.
+	assert(dynArrayIteratorHasNext(itr));
+	return (itr->da->data[itr->currentIndex++]);	// Return the current index, then update the pointer to the currentIndex.
 
 
 }
@@ -47,14 +50,15 @@ TYPE dynArrayIteratorNext (struct dynArrayIterator *itr) {
 void dynArrayIteratorRemove (struct dynArrayIterator *itr) {
 
 
+	removeAtDynArr(itr->da, itr->currentIndex);
+	if (itr->currentIndex > 0) {
+		itr->currentIndex--;
+	}
 
 
+}
 
+int main() {
 
-
-
-
-
-
-
+	return 0;
 }
--- week4/sheet_24.c
+++ week4/sheet_24.c
@@ -7,8 +7,8 @@ struct linkedListIterator {
 }
 
 void linkedListIteratorInit (struct linkedList  *lst, struct linkedListIterator * itr) {
-   itr->lst = lst;
-   itr->currentLink = lst->frontSentinel;
+    itr->lst = lst;
+    itr->currentLink = lst->frontSentinel;
 }
 
 void _removeLink (struct linkedList  *lst, struct dLink * lnk);
@@ -16,41 +16,30 @@ void _removeLink (struct linkedList  *lst, struct dLink * lnk);
 int linkedListIteratorHasNext (struct linkedListIterator *itr) {
 
 
-
-
-
-
+    int linkedListIteratorHasNext(struct linkedListIterator *itr) {
+        if (itr->currentLink->next != itr->lst->backSentinel) {
+            itr->currentLink = itr->lst->next;
+            return 1;
+        }
+        else {
+            return 0;
+        }
 
 
 }
 
 TYPE linkedListIteratorNext (struct linkedListIterator *itr) {
-
-
-
-
-
-
-
-
+	return itr->currentLink->value;
 
 }
 
-/* think about the next one carefully. Draw a picture. What should itr->currentLink be pointing to after the remove operation? */
+/* think about the next one carefully. Draw a picture. What should itr->currentLink 
+be pointing to after the remove operation? */
 
 void linkedListIteratorRemove (struct linkedListIterator *itr) {
    
-
-
-
-
-
-
-
-
-
-
-
-
+	struct dlink* tmp = itr->currentLink;
+	itr->currentLink = itr->currentLink->prev;
+	_removeLink(itr->lst, tmp);
 
 }
